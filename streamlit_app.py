b7ad05dd77d1144fa4d1ef3ac01c2d1ae5f33b9a
ksuzuka <ksuzuka@gmail.com>
2025-08-11 09:18:53 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,33 @@ import plotly.express as px
 
 df = pd.read_csv("state_data.csv")
 
-st.header("US States Demographics")
+st.header("Changes in US State Demographics Over Time")
 
-# Let user select which state to view
-state = st.selectbox("State:", df["State"].unique())
+# Let user select which state to view and which demographic data to view
+state = st.selectbox("State:", df["State"].unique()) 
+demographic = st.selectbox("Demographic:", ["Total Population", "Median Household Income"])
 
-# Create a graph of total population
-df_state = df[df["State"] == state]
-fig = px.line(
-    df_state, x="Year", y="Total Population", title=f"Total Population of {state}"
-)
-st.plotly_chart(fig)
-
+# Create tabs
+tab1, tab2 = st.tabs(["📈 Graph", "🔍 Data"],width="stretch")
+with tab1:
+# Create the graph the user requested
+        df_state = df[df["State"] == state]
+        if demographic == "Total Population":
+            fig = px.line(
+                    df_state, 
+                    x="Year", 
+                    y="Total Population", 
+                    title=f"Total Population of {state}",
+            )
+        elif demographic == "Median Household Income":
+            fig = px.line(
+                    df_state, 
+                    x="Year", 
+                    y="Median Household Income", 
+                    title=f"Median Household Income of {state}",
+            )
+        st.plotly_chart(fig)
+with tab2:
 # Show the entire dataframe
-st.write("All Data")
-st.dataframe(df)
+        st.write("All Data")
+        st.dataframe(df)
